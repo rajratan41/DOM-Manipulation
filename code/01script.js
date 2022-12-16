8a5ceae3c2cd1492b02ef5a53f9b676389985002
Raj Ratan <raj808341@gmail.com>
2022-12-16 19:41:59 +0530

    adding DOM Assignments

---
+++ code/01script.js
@@ -0,0 +1,13 @@
+// const title = document.querySelector (".side-bar .crayons-card .crayons-subtitle-2");
+// title.innerHTML = "Raj Ratan";
+
+// const discription = document.querySelector(".side-bar .crayons-card .color-base-70");
+// discription.innerHTML = "I am a Web Developer"
+
+let arr = ["aman", "naman"];
+let a =[];
+
+arr.forEach((val)=>{
+    a.push(val)
+});
+console.log(a);
